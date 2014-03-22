9edba403aef2cd421006742243a2bdf30fe0a3d5
Aaron Dufall <aald212@gmail.com>
2014-03-22 11:41:50 -0700

    Initial Commit

---
+++ Breakout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Breakout
+//
+//  Created by Aaron Dufall on 22/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Breakout/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Breakout
+//
+//  Created by Aaron Dufall on 22/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
